2516ee6a12bc3eed7b12b71561b250930297d945
xuelian <xldeng0808@gmail.com>
2024-01-08 10:59:49 -0800

    use react achieve tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const squares = [[1,2,3],[4,5,6],[7,8,9]]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GameView squares={squares}/>
     </div>
   );
+}
+
+const GameView = ({squares}) => {
+  const [status, setStatus] = useState('X')
+  const [values, setValues] = useState([])
+  const [isWin, setIsWin] = useState(false)
+  const getWinStatus = (newValue) => {
+    const lines = [
+      [1,2,3],
+      [4,5,6],
+      [7,8,9],
+      [1,4,7],
+      [2,5,8],
+      [3,6,9],
+      [1,5,9],
+      [3,5,7]
+    ]
+    for(let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if(newValue[a] === newValue[b] && newValue[b] === newValue[c]) return newValue[a]
+    }
+    return ''
+  }
+  const handleClick = (index) => {
+    if(values[index] || isWin) return;
+    const newValue = [...values]
+    newValue[index] = status
+    let winner = getWinStatus(newValue)
+    if(winner) {
+      setIsWin(true)
+    } else {  
+      const newStatus = status === 'X' ? 'O' : 'X'
+      setStatus(newStatus)
+    }
+    setValues(newValue)
+  }
+  return (
+    <div>
+      <div>
+        {isWin ? 'The Winner is:' + status : 'Current Player is:' + status}
+      </div>
+      {
+        squares.map((row,index) => (
+          <div className='board-row' key={index}>
+            {row.map(item => (
+              <Square key={item} onClick={() => handleClick(item)} text={values[item]}/>
+            ))}
+          </div>
+        ))
+      }
+      
+    </div>
+  )
+}
+
+const Square = ({onClick, text}) => {
+  const [clicked, setClicked] = useState(false)
+  const handleClick = (e) => {
+    setClicked(true)
+    onClick && onClick(e)
+  }
+  return(
+    <button onClick={handleClick} className='square-btn'>
+      {clicked && text }
+    </button>
+  )
 }
 
 export default App;
